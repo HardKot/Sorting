91c232c5c9bf4420ae94b33917230f00346ff434
HardKot <49830207+HardKot@users.noreply.github.com>
2019-08-01 00:55:37 +0500

    Add files via upload

---
+++ listtype.py
@@ -0,0 +1,28 @@
+types_beta = {'music' : ['aac', 'ac3', 'aif', 'aiff', 'amr', 'aob', 'ape', 'asf',
+                    'aud', 'awb', 'bin', 'bwg', 'cdr', 'flac', 'gpx', 'ics',
+                    'iff', 'm', 'm3u', 'm3u8', 'm4a', 'm4b', 'm4p', 'm4r',
+                    'mid', 'midi', 'mod', 'mp3', 'mpa', 'mpp', 'msc', 'msv',
+                    'nkc', 'ogg', 'ps', 'ra', 'ram', 'sdf', 'sib', 'sln', 'spl',
+                    'srt', 'temp', 'vb', 'wav', 'wave', 'wm', 'wma', 'wpd',
+                    'xsd', 'xwb'],
+         'pictures' : ['asf', 'bmp', 'cdw', 'cr2', 'cs', 'cur', 'dmp', 'drv', 'icns',
+                       'ico','jpg', 'max', 'mds', 'mng', 'msv', 'odt', 'pct', 'pict',
+                       'png', 'pps', 'prf', 'spl', 'tex', 'ttf', 'xps' ],
+         'documents': ['asp', 'cdd', 'cpp', 'doc', 'docm', 'docx', 'dot',
+                       'dotm', 'dotx', 'epub', 'fb2', 'gpx', 'ibooks', 'indd',
+                       'kdc', 'key', 'kml', 'mdb', ' mdf', 'mobi', 'mso', 'ods',
+                       'odt', 'one', 'oxps', 'pages', 'pdf', 'pkg', 'pl', 'pot',
+                       'potm', 'potx', 'pps', 'ppsm', 'ppsx', 'ppt', 'pptm',
+                       'pptx', 'ps', 'pub', 'rtf', 'sdf', 'sgml', 'sldm', 'snb',
+                       'wpd', 'wps', 'xar', 'xlr', 'xls', 'xlsb', 'xlsm','xlsx', 'xlt',
+                       'xltm', 'xltx', 'xps'],
+         'videos': ['3g2', '3gp', '3gp2', '3gpp', '3gpp2', 'asf', 'asx', 'avi',
+                    'bin', 'dat', 'drv', 'f4v', 'flv', 'gtp', 'h264', 'm4v',
+                    'mkv', 'mod', 'moov', 'mov', 'mp4', 'mpeg', 'mpg', 'mts',
+                    'rm', 'rmvb', 'spl', 'srt', 'stl', 'swf', 'ts', 'vcd',
+                    'vid', 'vod', 'webm', 'wm', 'wmv', 'yuv']}
+#ниже расширения с которыми я сталкивался, пока вижу смысл только в них. А верхний нужно сделать более 'красивым'
+types = {'music': ['mp3', 'ogg', 'wav', 'flac', 'wma'],
+         'pictures': ['jpg', 'png', 'bmp'],
+         'documents' ['docx', 'pptx', 'xlsx', 'pdf']: #пока не буду добавлять книги, для них стоит создать отдельную директорию. и заметки тоже
+         'videos': ['mp4', 'avi']}
